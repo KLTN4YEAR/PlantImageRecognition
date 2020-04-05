fa13fc332725089a6f84dff330d82ae338aa761c
Vu <nguyentuanvu231198@gmail.com>
2020-04-05 20:26:06 +0700

    Update structural

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const controllerUser = require('../controllers/user.controller');
+const controllerAuth = require('../controllers/auth.controller');
+const router = express.Router()
+
+const passport = require('passport');
+const passportConf = require('../passport'); //  de yen
+
+//login local
+router.route('/signin').post(controllerAuth.signin)
+    //signin gg
+router.route('/oauth/google').post(passport.authenticate('googleToken', { session: false }), controllerAuth.googleOAuth);
+router.route('/oauth/facebook').post(passport.authenticate('facebookToken', { session: false }), controllerAuth.facebookOAuth);
+module.exports = router;
