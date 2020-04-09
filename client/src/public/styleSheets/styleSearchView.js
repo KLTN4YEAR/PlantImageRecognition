83f6fe186ca8b3b561860ba6bc9d48c4cd07b2a5
Vu <nguyentuanvu231198@gmail.com>
2020-04-09 21:55:25 +0700

    Update Search Screen (Front)
    Search Screen includes:
    - Search Bar (like Header)
    - View Search
    - View Another Kind

---
+++ client/src/public/styleSheets/styleSearchView.js
@@ -0,0 +1,173 @@
+import { StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'transparent',
+    },
+    inputSearch: {
+        backgroundColor: 'transparent',
+    },
+    imgLogo: {
+        width: 200,
+        height: 200,
+        marginTop: 100,
+        marginBottom: 100,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+
+    },
+    viewSearch: {
+        marginTop: 0,
+    },
+    searchBar: {
+        backgroundColor: 'tomato',
+    },
+    inputSearchBar: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+    },
+    viewSafeArea: {
+        flex: 1,
+        marginTop: 5,
+    },
+    viewScroll: {
+        backgroundColor: 'transparent',
+        marginHorizontal: 5,
+
+    },
+    viewCard: {
+        width: 'auto',
+        height: 280,
+        marginTop: 10,
+        borderWidth: 1,
+        padding: 5,
+        margin: 3,
+        borderRadius: 3,
+        borderColor: '#d1d1d1',
+    },
+    viewPostBy: {
+        marginTop: 5,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        padding: 10,
+    },
+    rowPostBy: {
+        alignSelf: 'center',
+        alignItems: 'center',
+    },
+    txtUserName: {
+        fontSize: 14,
+        width: 120,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        fontWeight: 'bold',
+    },
+
+    viewImg: {
+        width: 'auto',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginBottom: 10,
+    },
+    viewDetail: {
+
+        position: 'relative',
+
+        display: 'flex',
+
+        margin: 10,
+
+    },
+
+    viewDetail: {
+        borderTopWidth: 1,
+        borderTopColor: '#d1d1d1',
+        position: 'relative',
+        display: 'flex',
+    },
+
+    btnEvent: {
+
+        width: 'auto',
+
+        height: 'auto',
+
+        margin: 25,
+
+        backgroundColor: '#d4d5d6',
+    },
+    txtName: {
+        width: '100%',
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginLeft: 'auto',
+        padding: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: '#d1d1d1',
+        marginRight: 'auto',
+        color: '#385898',
+    },
+    txtDec: {
+        width: '100%',
+        color: '#1c1e21',
+        fontFamily: 'Helvetica',
+        fontSize: 14,
+        maxHeight: 50,
+    },
+    imgCard: {
+        height: 180,
+        width: 180,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    viewPlant: {
+        width: '100%',
+        alignItems: 'center',
+        alignContent: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 3,
+        marginBottom: 3,
+    },
+    rowDetail: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        padding: 5,
+        marginTop: 10,
+    },
+    colDetail: {
+        width: '100%',
+        alignItems: 'center',
+        alignContent: 'center',
+    },
+
+    labelTxt: {
+        fontSize: 13,
+        width: '100%',
+        marginLeft: 5,
+        fontWeight: 'bold',
+
+    },
+    rowLabel: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    labelTxtContent: {
+        fontSize: 13,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    labelResult: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        padding: 10,
+        borderBottomColor: '#d1d1d1',
+        borderBottomWidth: 2,
+        width: '100%',
+        marginTop: 0,
+    },
+});
