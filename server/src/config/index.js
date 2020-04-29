e125b88095896264d8cf02c61c403eded03ac3d6
Vu <nguyentuanvu231198@gmail.com>
2020-04-29 22:02:41 +0700

    Upsdate Some FrontEnd & Test API

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,13 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/../.env` });
 
 const config = {
-    // env: process.env.NODE_ENV || 'development',
-    // port: process.env.PORT || 4000,
-    // jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    // mongoUri: process.env.MONGO_HOST ||
-    //     'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    //     (process.env.MONGO_PORT || '27017') +
-    //     '/PlantId',
     env: process.env.MONGOLAB_URI || process.env.NODE_ENV || 'development',
     port: process.env.PORT || 5000,
 
