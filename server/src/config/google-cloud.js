ffce8aa34127ea9993c0026e2e9274ceb913bb6f
huyhoang1001 <huyhoangpro1001@gmail.com>
2020-04-29 14:43:28 +0700

    update

--- server/src/config/google-cloud.js
+++ server/src/config/google-cloud.js
@@ -1,6 +1,7 @@
-const Cloud = require('@google-cloud/storage')
-const path = require('path')
-const serviceKey = path.join(__dirname, './keys.json')
+const Cloud = require('@google-cloud/storage');
+const path = require('path');
+
+const serviceKey = path.join(__dirname, './keys.json');
 const { Storage } = Cloud
 const storage = new Storage({
   keyFilename: serviceKey,
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,15 +1,19 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/../.env` });
+
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
+
   mongoUri: process.env.MONGOLAB_URI ||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/RecognitionPlant',
+
   tokenLife: '1h',
   jwtSecret: 'your tokenSerect',
+
   oauth: {
     google: {
       clientID: '242010101479-3agf57mh6kaoagi90mi154k8l6i9ouf2.apps.googleusercontent.com',
