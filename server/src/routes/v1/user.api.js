e9143350dce905b7aaebb2447cd749ae6bcf20cb
huyhoang1001 <huyhoangpro1001@gmail.com>
2020-05-02 22:54:04 +0700

    fix get info user

--- server/src/routes/v1/user.api.js
+++ server/src/routes/v1/user.api.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { controllerUser } = require('../../controllers');
 const authMiddleware = require('../../middlewares/auth.middleware');
 
-const post_api_route = express.Router();
+const user_api_route = express.Router();
 
-post_api_route.route('/getInfo/:_id').get(authMiddleware, controllerUser.getInfo);
+user_api_route.route('/getInfo/:_id').get(authMiddleware, controllerUser.getInfo);
 
-module.exports = post_api_route;
+module.exports = user_api_route;
