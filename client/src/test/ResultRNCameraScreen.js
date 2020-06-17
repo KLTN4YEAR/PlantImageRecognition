9e00c8175bd3e179c6be265b31af91c8733ea3eb
vunt <vunt@solazu.com>
2020-06-17 22:01:47 +0700

    Update new model and new data

--- client/src/test/ResultRNCameraScreen.js
+++ client/src/test/ResultRNCameraScreen.js
@@ -39,8 +39,8 @@ class ResultCamera extends Component {
     this.setState({model});
     switch (model) {
       case flower:
-        var modelFile = 'models/lite_flowers_model_v5.tflite';
-        var labelsFile = 'models/lite_flowers_model_v5.txt';
+        var modelFile = 'models/lite_flowers_model_v7.tflite';
+        var labelsFile = 'models/lite_flowers_model_v7.txt';
         break;
       default:
         var modelFile = 'models/lite_flowers_model_v1.tflite';
